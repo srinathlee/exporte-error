71caf875917f0bbb607aebeb6ca8743b6e36a9b8
srinath5255 <iamsrinath5255@gmail.com>
2023-05-15 05:43:44 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import {Heading} from './syledComponent'
+import './App.css'
+
+const App = () => <Heading type="button">hello</Heading>
+
+export default App
---
+++ src/syledComponent.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const Heading = styled.h1`
+  color: #2320f5;
+  font-family: 'Roboto';
+`
